1b17cdfe6dff6cfa3ec04374d842b91d3e6e5936
Mikhail <mixail.workmail@yandex.ru>
2020-03-15 20:24:59 +0400

    add comments and bootstrap example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+// JS - ./js/common.js
 import './js/common'
+// CSS (example)
 import './css/main.css'
+// SCSS
 import './scss/main.scss'
+
+// Bootstrap (example)
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
