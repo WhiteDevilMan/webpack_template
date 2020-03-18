cdb593d2440d9e217794697e9fe4afb17ace1e37
Mikhail <mixail.workmail@yandex.ru>
2020-03-18 13:49:23 +0400

    data component + store

--- src/index.js
+++ src/index.js
@@ -12,9 +12,16 @@ import './scss/main.scss'
 // import 'vue'
 // import Vue from 'vue'
 window.Vue = require('vue')
+import store from './store'
 
 Vue.component('example-component', require('./components/Example.vue').default)
 
 const app = new Vue({
+  data () {
+    return {
+      component: false,
+    }
+  },
+  store,
   el: '#app'
 })
