12fdd96d137427495e41e3f09a8a907692ecc945
Mikhail <mixail.workmail@yandex.ru>
2020-05-12 13:22:14 +0400

    change css & scss import paths

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // JS - ./js/common.js
 import './js/common'
 // CSS (example)
-import './css/main.css'
+import './assets/css/main.css'
 // SCSS
-import './scss/main.scss'
+import './assets/scss/main.scss'
 
 // Bootstrap (example)
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
